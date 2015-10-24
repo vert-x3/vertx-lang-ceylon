c9342cfd8537d713f3ee534cc0269f125d296750
Julien Viet <julien@julienviet.com>
2015-10-24 13:01:10 +0200

    Fix compilation scripts

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
-find src/main/ceylon -name *.ceylon | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon
-find src/main/ceylon -name *.java | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon
+ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon io.vertx.lang.ceylon
+find src/main/ceylon/io/vertx/ceylon -name *.ceylon | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon
+find src/main/ceylon/io/vertx/ceylon -name *.java | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon
--- test-compile.sh
+++ test-compile.sh
@@ -1,2 +1,3 @@
-find src/test/ceylon -name *.ceylon | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon
-find src/test/ceylon -name *.java | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon
+find src/test/ceylon/io/vertx/ceylon -name *.ceylon | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon
+find src/test/ceylon/io/vertx/ceylon -name *.java | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon
+ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon io.vertx.tests
