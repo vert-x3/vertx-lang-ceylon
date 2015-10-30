df1aadc11ac5763fa8c3d983c9c40b2e6ef332a2
Julien Viet <julien@julienviet.com>
2015-10-30 22:01:39 +0100

    Use plugin for imports

--- compile.sh
+++
@@ -1,3 +0,0 @@
-ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon io.vertx.lang.ceylon
-find src/main/ceylon/io/vertx/ceylon -name *.ceylon | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon
-find src/main/ceylon/io/vertx/ceylon -name *.java | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/main/ceylon
--- test-compile.sh
+++
@@ -1,3 +0,0 @@
-find src/test/ceylon/io/vertx/ceylon -name *.ceylon | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon
-find src/test/ceylon/io/vertx/ceylon -name *.java | xargs ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon
-ceylon compile --suppress-warning --javac=-target=8 --source=src/test/ceylon io.vertx.tests
